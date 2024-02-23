c4807f2bf63bf7562c042407c326c5615b3d38aa
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-02-23 11:32:31 -0500

    Trying to submit again.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 1:
+        print([0])
+        return
+    if length == 0:
+        print([])
+        return
+    fibonacci_sequence = [0,1]
+    while len(fibonacci_sequence) < length:
+        next_fibonacci = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_fibonacci)
+    print(fibonacci_sequence)
+
+print_fibonacci(1)
